7a5583e1254ee7f9ed65dad53252d312f9abb4bc
Maxime <91959894+MaximeMonterin@users.noreply.github.com>
2022-03-28 17:19:21 +0200

    Add files via upload

---
+++ java-workspace/GameMaker/jeu/Objet.java
@@ -0,0 +1,19 @@
+package jeu;
+
+public class Objet {
+	private String name;
+
+	public Objet(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
